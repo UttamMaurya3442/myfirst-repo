b66c459259035e6c978594d79041ca6a1114dcdf
Uttam Maurya <sologamersrajan@gmail.com>
2025-05-25 18:11:07 +0530

    initaial commit

--- 01_python1.py
+++ 01_python1.py
@@ -1,1 +1,56 @@
 print("hello")
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>My Digital Business Card</title>
+
+  </head>
+
+  <body>
+
+    <h1>John Doe</h1>
+
+    <h2>Web Developer</h2>
+
+ 
+
+    <h3>Contact Information:</h3>
+
+    <ul>
+
+      <li>Email: john.doe@example.com</li>
+
+      <li>Phone: (123) 456-7890</li>
+
+      <li>Location: New York, NY</li>
+
+    </ul>
+
+ 
+
+    <h3>Skills:</h3>
+
+    <ol>
+
+      <li>HTML</li>
+
+      <li>CSS</li>
+
+      <li>JavaScript</li>
+
+    </ol>
+
+ 
+
+    <p>Connect with me on <a href="https://www.linkedin.com">LinkedIn</a></p>
+
+  </body>
+
+</html>
